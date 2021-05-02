abad61c156583dec1c244c2d3db92c0b87347107
Alaa Hamoudah <alaakhalil11@gmail.com>
2021-05-02 16:15:13 +0300

    some error code

--- test_sum.py
+++ test_sum.py
@@ -5,7 +5,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")\
 
   def test_sum_with_negatives(self):
-        self.assertEqual(sum([-4, -4, -4]), -12, "Should be -12")
+        self.assertEqual(sum([-4, -4, -4]), 12, "Should be -12")
 
   def test_sum_with_zeros(self):
         self.assertEqual(sum([0, 0, 0]), 0, "Should be zero")
